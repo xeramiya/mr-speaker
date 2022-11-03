1ff1c86b4ef07bd10a9b9aea78928a0a6742001d
STr3292 <str3292@gmail.com>
2022-11-03 13:36:56 +0900

    relocate audio data

--- asset/link/linkData.js
+++ asset/link/linkData.js
@@ -8,7 +8,7 @@ module.exports.librarian = (index) => {
   const shelf = [
   {
     name: 'aot23',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2F%E3%82%A6%E3%82%A8%E3%83%92%E3%82%99%E3%83%83%E3%83%88%E7%A4%BE%E5%AE%9A%E6%99%82%E6%94%BE%E9%80%81.ogg?v=1588249193404'
+    link: 'asset/audio/sound/system_sound/AoT23.mp3'
   },
   {
     name: 'diskSystem',
@@ -16,107 +16,99 @@ module.exports.librarian = (index) => {
   },
   {
     name: 'kbc',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2F%E3%82%A4%E3%82%B9%E3%83%A9%E3%82%A8%E3%83%AB%E3%81%AB%E3%81%A8%E3%81%A3%E3%81%A8%E3%81%A8%E3%82%B9%E3%83%92%E3%82%9A%E3%83%B3.mp3?v=1592481664996'
+    link: 'asset/audio/sound/system_sound/kbc.mp3'
   },
   {
     name: 'hamburg',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2Fsisyo.mp3?v=1609674269671'
+    link: 'asset/audio/sound/system_sound/sisyo.mp3'
   },
   {
     name: 'power',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FOldSpice.mp3?v=1609675229182'
+    link: 'asset/audio/sound/system_sound/power.mp3'
   },
   {
     name: 'beaver',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2Fbeaver.mp3?v=1609671671760'
+    link: 'asset/audio/sound/system_sound/beaver.mp3'
   },
   {
     name: 'pikachu',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FPikachu.mp3?v=1612181536593'
+    link: 'asset/audio/sound/system_sound/pikachu.mp3'
   },
   {
     name: 'challengerGingle',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FChallenger_Gingle.mp3?v=1613390764092'
+    link: 'asset/audio/sound/system_sound/Challenger_Gingle.mp3'
   },
   {
     name: 'dddGingle',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FDDD_Gingle.mp3?v=1613390763592'
+    link: 'asset/audio/sound/system_sound/DDD_Gingle.mp3'
   },
   {
     name: 'kokosuki',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FKOKOSUKI.mp3?v=1613729517078'
+    link: 'asset/audio/sound/system_sound/KOKOSUKI.mp3'
   },
   {
     name: 'wakaruman',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2Fwakaruman.mp3?v=1613739866462'
+    link: 'asset/audio/sound/system_sound/wakaruman.mp3'
   },
   {
     name: 'ehettenandayo',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FEhe%20tte%20Nandayo.mp3?v=1613739880224'
+    link: 'asset/audio/sound/system_sound/Ehe_tte_Nandayo.mp3'
   },
   {
     name: 'switch',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FSwitch_SE.mp3?v=1614688336961'
-  },
-  {
-    name: 'peacefullTimesQ',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FpeacefulTimesQ.mp3?v=1614699043024'
-  },
-  {
-    name: 'peacefullTimesS',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FpeacefulTimesS.mp3?v=1614853947285'
+    link: 'asset/audio/sound/system_sound/Switch_SE.mp3'
   },
   {
     name: 'kyouheiShibata',
-    link: "https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FKyouheiShibata's_RunningStyle.mp3?v=1614840552400"
+    link: "asset/audio/sound/system_sound/KyouheiShibata's_RunningStyle.mp3"
   },
   {
     name: 'killYou',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FkillYou.mp3?v=1617282190828'
+    link: 'asset/audio/sound/system_sound/killYou.mp3'
   },
   {
     name: 'fireMario',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FfireMario.mp3?v=1618502913593'
+    link: 'asset/audio/sound/system_sound/fireMario.mp3'
   },
   {
     name: 'yoshi90',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FYOSHI90.mp3?v=1623762684444'
+    link: 'asset/audio/sound/system_sound/YOSHI90.mp3'
   },
   {
     name: 'yamashita',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2F2EM09_Yamahita.mp3?v=1613846874618'
+    link: 'asset/audio/music/2EM09Yamashita.mp3'
   },
   {
     name: 'themeOf428',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2F428.ogg?v=1590348288922'
+    link: 'asset/audio/music/428.mp3'
   },
   {
     name: 'lifeOnMars',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FLife-On-Mars%3F.ogg?v=1591803839472'
+    link: 'asset/audio/music/LifeOnMars.mp3'
   },
   {
     name: 'twilight',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2F02%20Twilight.mp3?v=1612180895908'
+    link: 'asset/audio/music/Twilight.wav'
   },
   {
     name: 'emergencyMeeting',
-    link: 'https://cdn.glitch.me/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2FEMERGENCY_MEETING-SE.mp3?v=1639379028697'
+    link: 'asset/audio/sound/system_sound/EMERGENCY_MEETING-SE.mp3'
   },
   {
     name: '#11above',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2F11above.ogg?v=1588065381293'
+    link: 'asset/audio/music/#11above.mp3'
   },
   {
     name: 'abe',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2F%E5%AE%89%E5%80%8D%E6%99%8B%E4%B8%89.ogg?v=1588595064568'
+    link: 'asset/audio/sound/system_sound/abe.wav'
   },
   {
     name: 'koike',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2F%E5%B0%8F%E6%B1%A0%E7%99%BE%E5%90%88%E5%AD%90.ogg?v=1588595064970'
+    link: 'asset/audio/sound/system_sound/koike.wav'
   },
   {
     name: 'valve',
-    link: 'https://cdn.glitch.com/3e3ea0c8-83cc-4a76-bb05-c9000eb70665%2Fvalve.ogg?v=1589352035171'
+    link: 'asset/audio/sound/system_sound/Valve.mp3'
   },
   {
     name: 'xpShutdown',
